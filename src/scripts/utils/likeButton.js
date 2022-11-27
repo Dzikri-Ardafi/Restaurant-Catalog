acdfc41196e2fc8827c05ebe2e253fc80c731530
Dzikri Ardafi <dzkri.ardfi@gmail.com>
2022-11-27 22:36:48 +0700

    edit button like

--- src/scripts/utils/likeButton.js
+++ src/scripts/utils/likeButton.js
@@ -1,12 +1,10 @@
 const buttonLikeSession = () => `
     <button aria-label = "like this restaurant" id="likeButton" class="likeButton">
-    hy
     <i class="fa fa-heart-o" area-hidden="true"></i>
   </button>
     `;
 const buttonUnlikeSession = () => `
     <button aria-label = 'unlike this restaurant' id="likeButton" class = "likeButton">
-    hy
         <i class="fa fa-heart" area-hidden='true'></li>
     </button>
 `;
